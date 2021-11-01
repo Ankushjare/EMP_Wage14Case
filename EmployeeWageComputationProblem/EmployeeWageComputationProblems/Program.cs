3f2f421cf171280dc004c4af860fbe172843f7a9
Ankushjare <jareankush@gmail.com>
2021-11-01 14:33:16 +0530

    Wage For Each Company

--- EmployeeWageComputationProblem/EmployeeWageComputationProblems/Program.cs
+++ EmployeeWageComputationProblem/EmployeeWageComputationProblems/Program.cs
@@ -6,8 +6,12 @@ namespace EmployeeWageComputationProblems
     {
         static void Main(string[] args)
         {
-            RefactorCode obj7 = new RefactorCode();
-            obj7.computeEmpWage();
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
         }
     }
 }
